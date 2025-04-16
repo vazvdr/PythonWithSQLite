e1e1996bf8090a8e55234240af4e7fe010da3966
Vanderson de Azevedo <127865294+vazvdr@users.noreply.github.com>
2025-04-16 20:23:54 -0300

    Sending classes pessoa, marca e veiculo

---
+++ main.py
@@ -0,0 +1,14 @@
+from datetime import date
+from pessoa import Pessoa
+from marca import Marca
+from veiculo import Veiculo
+
+pessoa1 = Pessoa(cpf=12345678900, nome="Vanderson", nascimento=date(1991, 1, 13), oculos=True)
+
+marca1  = Marca(id=1, nome="Fiat", sigla="FIA")
+
+veiculo1= Veiculo(placa="KMN-2030", cor="preto", proprietario=pessoa1, marca=marca1)
+
+print(pessoa1)
+print(marca1)
+print(veiculo1)
