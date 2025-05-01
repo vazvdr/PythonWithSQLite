37fb76c3c4c99b1d90558c46726a62866d552b41
Vanderson de Azevedo <127865294+vazvdr@users.noreply.github.com>
2025-04-30 21:02:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from bancodedados import BancoDeDados
+from banco import Bn
 from pessoa import Pessoa
 from marca import Marca
 from veiculo import Veiculo
@@ -13,4 +13,22 @@ if __name__ == "__main__":
 pessoa1 = Pessoa(cpf=12345678900, nome="Vanderson", nascimento=date(1991, 1, 13), oculos=True)
 banco.inserir_pessoa(pessoa1)
 
-print(pessoa1)
+marca1  = Marca(id=1, nome="Fiat", sigla="FIA")
+banco.inserir_marca(marca1)
+
+veiculo1= Veiculo(placa="KMN-2030", cor="preto", proprietario=pessoa1, marca=marca1)
+banco.inserir_veiculo(veiculo1)
+
+print("Pessoas: ")
+for pessoa in banco.buscar_todas_pessoas():
+    print(pessoa)
+
+print("\nMarcas: ")
+for marca in banco.buscar_todas_marcas():
+    print(marca)
+
+print("\nVeículos: ")
+for veiculo in banco.buscar_todos_veiculos():
+    print(veiculo)
+
+banco.fechar_conexao()
